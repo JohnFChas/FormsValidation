f2bb0e381642679ad9e63cd339d8037a00cd64da
John Fristedt <john.fristedt@chas.se>
2016-10-21 11:30:34 +0200

    Initial Commit

--- Scripts/Controllers/home.controller.js
+++ Scripts/Controllers/home.controller.js
@@ -5,5 +5,30 @@ angular.module("mainModule")
         "$scope",
         function ($scope) {
             $scope.title = "Home";
+
+            $scope.formInputs = [{
+                type: "text",
+                id: "name",
+                label: "Name",
+                model: "John",
+                isValid: false
+            }, {
+                type: "number",
+                id: "age",
+                label: "Age",
+                min: 0,
+                model: 0,
+                isValid: false
+            }, {
+                type: "email",
+                id: "email",
+                label: "E-Mail",
+                model: "john@john.john",
+                isValid: false
+            }];
+
+            $scope.submitForm = function () {
+                alert("TODO: Submit form properly");
+            }
         }
     ]);
